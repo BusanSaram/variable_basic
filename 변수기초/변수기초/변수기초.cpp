9a9e3185f75123d1f0b73635e9292c791890c10a
kyungmin <gogoabroad6817@gmail.com>
2026-08-26 08:04:53 -0400

    I learned about data types such as char, short, int, __int64 and how memory works

---
+++ 변수기초/변수기초/변수기초.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+//char 대략적으로 -100에서 100
+//short 대략적으로 -30000에서 30000까지 
+//int (4byte) -21억에서 21까지 
+//__int64 (8byte) 21억을 초과하면 __int64로 가면 된다라고 보면 된다. 
+int hp; // 그리고 30000이면 아쉽고 21억 정도면 충분히 HP를 표현할 수 있겠다라고 생각이 들어서 int를 쓴다.
+
+//Data영역
+
+int main() // main으로 들어와 실행이 된다. 
+{
+    hp = 50; // 50을 hp에 넣는다.
+
+    //Stack 영역
+    cout << "Hello World!\n";
+}
