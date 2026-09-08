be7485ce05a87ba4711b56276277cbe3a948c952
kyungmin <gogoabroad6817@gmail.com>
2026-09-08 07:49:30 -0400

    Assignment Operator(대입연산)

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -2,11 +2,14 @@
 
 using namespace std;
 
-//float speed; //4byte
-//double speed2;
-bool playSound;
+//대입연산
+int a;
+int b;
 
 int main(){
-	playSound = false;
-	playSound = true;
+
+	//cout << (a = 10); // a에 10을 넣고 a의 값이 뭔지 질의를 하는 것임 또는 
+	a = b = 10; //이렇게 연쇄작용으로 값을 넣을 수 있다.
+	cout << a << endl;
+	cout << b;
 }
