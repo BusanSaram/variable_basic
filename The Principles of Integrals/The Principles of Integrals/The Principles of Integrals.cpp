92341cdcdc7c483773739ccb3114bb9f9b2b1269
kyungmin <gogoabroad6817@gmail.com>
2026-09-08 08:42:30 -0400

    increment, decrement(증감)

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -2,14 +2,11 @@
 
 using namespace std;
 
-//대입연산
-int a;
-int b;
-
+//사칙연산 
+int hp;
 int main(){
-
-	//cout << (a = 10); // a에 10을 넣고 a의 값이 뭔지 질의를 하는 것임 또는 
-	a = b = 10; //이렇게 연쇄작용으로 값을 넣을 수 있다.
-	cout << a << endl;
-	cout << b;
+	hp = hp++; // 이 줄 끝나고 증가
+	hp + 1; //증감
+	hp = ++hp; // 바로 증가
 }
+ 
