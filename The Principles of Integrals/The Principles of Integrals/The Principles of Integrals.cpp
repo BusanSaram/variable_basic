cc2d734929e86adb18865a251b16753601bef876
kyungmin <gogoabroad6817@gmail.com>
2026-09-08 10:16:35 -0400

    branch 분기문

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -5,34 +5,23 @@ using namespace std;
 //논리연산
 //비밀번호도 맞아야 하지만 아이디도 맞아 떨어져야지 로그인이 가능하다.
 
-bool isRich;
-bool isTall;
+// 분기문과 반복문만 잘하면 어떠한 코드도 거의 만들 수 있다.
+
+int hp;
 
 int main(){
-
-	//비교연산
-	// == 같은지
-	//result = (a==b);
-
-	//<
-	//<=
-	//>
-	//>=
-
-
-	//논리 연산
-	isRich = true;
-
-	!isRich;// true의 반대는 false
-	cout << !isRich << endl;
-
-	isRich&& isTall; //이 두개를 만족해야지 true;
-	//근데 키작은 사람이 좋다 하면
-	isRich && !isTall;
-	isRich && (isTall == false); //이것도 같다. 
-
-	// ! not
-	// && and 이고
-	// || or 이면
+	hp = 100;
+	if (hp <= 0) 
+	{ // 보통 게임 업계에선 이런 식으로 코드를 짠다.중괄호를 if 밑으로 가게함
+		cout << "몬스터를 처치" << endl;
+	}
+	else {
+		if (hp <= 30) {
+			cout << "몬스터가 도망감" << endl;
+		}
+		else {
+			cout << "몬스터가 반격함" << endl;
+		}
+	}
 }
  
