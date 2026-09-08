6b16e19b1333f090c1c876d9478ac4b484f60080
kyungmin <gogoabroad6817@gmail.com>
2026-09-08 10:49:05 -0400

    구구단(Multiplication table)

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -2,18 +2,20 @@
 
 using namespace std;
 
-// 별찍기 연습문제
+// 구구단
 
 int main(){
-	//키보드 입력 가능
-	int N;
-	cin >> N;
-	
-	for (int j = 0; j < N; j++) {
-		for (int i = 0; i < N; i++) {
-			cout << '*';
+	//2*1 =2
+	//2*2 = 4
+	//2*9 = 18
+	//9*9 = 81;
+		
+	for (int j = 2; j < 10; j++) {
+		for (int i = 1; i < 10; i++) {
+			cout << i*j << " ";
 		}
 		cout << endl;
 	}
+
 }
  
