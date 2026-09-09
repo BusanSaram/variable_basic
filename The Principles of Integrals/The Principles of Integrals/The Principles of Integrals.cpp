45428634a82d9baa0b1c165f6ea0f3b1102e7c43
kyungmin <gogoabroad6817@gmail.com>
2026-09-09 09:35:48 -0400

    same name but computer think these are different functions

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -1,17 +1,22 @@
 ﻿#include <iostream>
 using namespace std;
 
-
-int Add(int a, float b);
-
-int main(){
-	int result = Add(10, 20);
-	return 0;
-}
-
+//같은 이름의 함수지만 완전히 다른 함수라고 인지를 한다. argument 갯수, 타입이 다르기 때문이다.
 int Add(int a, float b)
 {
 	int c = a + b;
 	cout << c << endl;
 	return c;
 }
+
+int Add(int a)
+{
+	return 0;
+}
+
+
+int main(){
+	int result = Add(10, 20);
+	return 0;
+}
+
