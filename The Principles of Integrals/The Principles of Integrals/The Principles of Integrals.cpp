4aa6098dcb3c0a777868ed66606704d995f772bf
kyungmin <gogoabroad6817@gmail.com>
2026-09-09 09:25:52 -0400

    int Add(int a, float b) there is return value(return c) to use it at the main function.

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -2,19 +2,14 @@
 using namespace std;
 
 
-//보통 함수 쓸 때 함수 이름을 대문자로 적는다.
-
-//입력할게 없으면 굳이 void TestFunction(void)할 필요 없다. 
-void TestFunction(int a, float b) {
-	cout << a << b << endl;
-	return;
+int Add(int a, float b) 
+{
+	int c = a + b;
+	cout << c << endl;
+	return c;
 }
 
 int main(){
-	int a;
-	cin >> a;
-	float b;
-	cin >> b;
-	TestFunction(a,b);
+	int result = Add(10, 20);
 	return 0;
 }
