099bfa5a7dfd05ed49b355f81cc86fb12fb098d0
kyungmin <gogoabroad6817@gmail.com>
2026-09-09 09:31:46 -0400

    I can type the functions if there is signature function in front of the main function

--- The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
+++ The Principles of Integrals/The Principles of Integrals/The Principles of Integrals.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 
-int Add(int a, float b) 
-{
-	int c = a + b;
-	cout << c << endl;
-	return c;
-}
+int Add(int a, float b);
 
 int main(){
 	int result = Add(10, 20);
 	return 0;
+}
+
+int Add(int a, float b)
+{
+	int c = a + b;
+	cout << c << endl;
+	return c;
 }
